0bd40137ab96c6fdf9e96b98dec3188ab0a670ea
hiurydev <81237941+hiurydev@users.noreply.github.com>
2021-09-16 21:35:14 -0300

    Criado listagem e delete de categoria

--- src/main/java/br/senac/devweb/api/product/categoria/CategoriaRepository.java
+++ src/main/java/br/senac/devweb/api/product/categoria/CategoriaRepository.java
@@ -1,10 +1,13 @@
 package br.senac.devweb.api.product.categoria;
 
+import com.querydsl.core.types.Predicate;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
 public interface CategoriaRepository extends CrudRepository<Categoria, Long>,
         QuerydslPredicateExecutor<Categoria> {
+
+    List<Categoria> findAll(Predicate filter);
 }
